c093f695e81a3a89b158b504ed07296286bacef1
misogil0116 <lack_un@yahoo.co.jp>
2022-12-12 17:30:58 +0900

    implementing outputting the json

--- bacon/bacon.py
+++ bacon/bacon.py
@@ -3,7 +3,8 @@ from pdf_analyzer import PDFAnalyzer
 from coordinate_helper import (
     convert_bbox_mediabox, 
     convert_char_bboxes_to_raw_img_size,                        
-    convert_layouts_to_raw_img_size
+    convert_layouts_to_raw_img_size,
+    compute_overlap
 )
 from visualizer import visualize
 
@@ -21,10 +22,44 @@ class bacon:
                        (255,0,255), (128,128,0), (0,128,128), (128,0,128), (128,0,0),
                        (0,0,128)]
         self.pred_image_size = (1025, 1025) # paper definition
+        self.overlap_threshold = 0.8
+
+    def is_text_category(self, category_id):
+        return self.categories[category_id.item()] in ['Caption', 'Footnote', 'List-item', 'Page-footer',
+                                                       'Page-header', 'Section-header', 'Table', 'Text', 'Title']
+
+    def is_overlapped_threshold(self, pred_box, char_box):
+        char_area = (char_box[2] - char_box[0]) * (char_box[3] - char_box[1])
+        overlapped_area = compute_overlap(pred_box, char_box)
+        return overlapped_area/char_area < self.overlap_threshold
+
+    def group_up_with_box(self, pred_box, char_boxes):
+        outputs = []
+
+        # filter overlapped char boxes to pred_box
+        no_overlapped_chars = [(char, char_box) for char, char_box in char_boxes if self.is_overlapped_threshold(pred_box, char_box)]
+        return no_overlapped_chars
 
     def integrate_chars_with_layouts(self, bbox_dict_list, layouts, image_sizes):
         char_boxes_list, layouts = self.scale_raw_img_size(bbox_dict_list, layouts, image_sizes)
-        return char_boxes_list, layouts
+        outputs = []
+        for char_boxes, layout in zip(char_boxes_list, layouts):
+            pred_boxes = layout['instances'].pred_boxes
+            pred_categories = layout['instances'].pred_classes
+
+            for pred_box, pred_category in zip(pred_boxes, pred_categories):
+                if self.is_text_category(pred_category):
+                    box_output = self.group_up_with_box(pred_box, char_boxes)
+                    return box_output
+                    #outputs.append(box_output)
+                else:
+                    box_output = {
+                        'coordinate': pred_box.tolist(),
+                        'category': self.categories[pred_category.item()],
+                        'text': None
+                    }
+                    outputs.append(box_output)
+        return outputs
 
     def scale_raw_img_size(self, bbox_dict_list, layouts, image_sizes):
         scaled_char_boxes_list, scaled_layouts = [], []
@@ -44,8 +79,10 @@ class bacon:
         bbox_dict_list = self.pdf_analyzer.extract_char_bbox(filename)
 
         # integrate the results
-        char_bboxes, layouts = self.integrate_chars_with_layouts(bbox_dict_list, layouts, image_sizes)
-        visualize(char_bboxes[0], layouts[0], pdf_images[0], self.categories, self.colors)
+        #char_bboxes, layouts = self.integrate_chars_with_layouts(bbox_dict_list, layouts, image_sizes)
+        no_overlapped_chars = self.integrate_chars_with_layouts(bbox_dict_list, layouts, image_sizes)
+        
+        visualize(no_overlapped_chars, layouts[0], pdf_images[0], self.categories, self.colors)
         #output = self.jsonify(layouts_with_texts)
         #return output
 
--- bacon/coordinate_helper.py
+++ bacon/coordinate_helper.py
@@ -19,7 +19,14 @@ def convert_bbox_mediabox(bbox, mediabox, image_size):
     x1, y1, x2, y2 = bbox
     x1, x2 = scale([x1, x2], (image_size[0]/mediabox[0]))
     y1, y2 = scale([y1, y2], (image_size[1]/mediabox[1]))
-    return [x1, image_size[1]-y1, x2, image_size[1]-y2]
+    return [x1, image_size[1]-y2, x2, image_size[1]-y1]
     
 def scale(xy, ratio_xy):
     return [xy[0]*ratio_xy, xy[1]*ratio_xy]
+
+def compute_overlap(pred_box, char_box):
+    x1, y1, x2, y2 = pred_box.tolist()
+    hat_x1, hat_y1, hat_x2, hat_y2 = char_box
+    w = min(hat_x2, x2) - max(hat_x1, x1)
+    h = min(hat_y2, y2) - max(hat_y1, y1)
+    return max(w*h, 0)
